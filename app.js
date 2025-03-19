201988ce865c87039d5fae48853f295876ad8435
mirinae700 <mirinae219@gmail.com>
2025-03-19 11:42:01 +0900

    feat: 라우팅 기능 추가, DOM API 완전 제거

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const container = document.getElementById('root');
-const content = document.createElement('div');
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
@@ -10,32 +9,50 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function router() {
+  const path = location.hash; // #만 있을 경우 빈값을 반환한다
 
-window.addEventListener('hashchange', () => {
+  if (path === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+function newsDetail() {
   const id = location.hash.substring(1);
 
   const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = document.createElement('h1');
-  title.textContent = newsContent.title;
 
-  content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-
-  div.innerHTML = `
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
+  container.innerHTML = `
+  <h1>${newsContent.title}</h1>
+  
+  <div>
+  <a href="#">목록으로</a>
+  </div>
   `;
-
-  ul.appendChild(div.firstElementChild);
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsFeed() {
+  const newsList = [];
+  const newsFeed = getData(NEWS_URL);
+
+  newsList.push('<ul>');
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+      <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+      </li>
+      `);
+  }
+
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');
+}
+
+window.addEventListener('hashchange', router);
+router();
